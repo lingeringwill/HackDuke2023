c97585afddc4b4220112ae0178bfa714743b9c1f
kenny <kennycs@protonmail>
2023-09-09 14:24:49 -0400

    Added randomly generated points for testing

--- map/index.js
+++ map/index.js
@@ -9,23 +9,34 @@ script.async = true;
 window.initMap = async function() {
     const { Map } = await google.maps.importLibrary("maps");
 
+    function getRandomLatLng() {
+      const minLat = 24.396308; 
+      const maxLat = 49.384358;
+      const minLng = -125.000000; 
+      const maxLng = -66.934570;
+      
+      const lat = Math.random() * (maxLat - minLat) + minLat;
+      const lng = Math.random() * (maxLng - minLng) + minLng;
+    
+      return { lat, lng };
+    }
+    
+    function getSmoothWeight() {
+      return Math.random() * 100;
+    }
+    
+    const points = [];
+    for (let i = 0; i < 1000; i++) {
+      const { lat, lng } = getRandomLatLng();
+      const weight = getSmoothWeight();
+      const point = {
+        location: new google.maps.LatLng(lat, lng),
+        weight: weight.toFixed(6)
+      };
+      points.push(point);
+    }
+
     // bigger weight = more red
-
-    var heatMapData = [
-      {location: new google.maps.LatLng(32.806671, -86.791130), weight: 4}, // alabama
-      {location: new google.maps.LatLng(61.370716, -152.404419), weight: 8}, // alaska
-      {location: new google.maps.LatLng(33.729759, -111.431221), weight: 5}, // arizona
-      {location: new google.maps.LatLng(34.969704, -92.373123), weight: 3}, // arkansas
-      {location: new google.maps.LatLng(36.116203, -119.681564), weight: 5}, // california
-      {location: new google.maps.LatLng(39.059811, -105.311104), weight: 3}, // colorado
-      {location: new google.maps.LatLng(41.597782, -72.755371), weight: 2}, // conneticut
-      {location: new google.maps.LatLng(39.318523, -75.507141), weight: 2}, // delaware 
-      {location: new google.maps.LatLng(38.897438, -77.026817), weight: 3}, // district of columbia
-      {location: new google.maps.LatLng(27.766279, -81.686783), weight: 10}, // florida
-      {location: new google.maps.LatLng(33.040619, -83.643074), weight: 4}, // georgia
-      {location: new google.maps.LatLng(21.094318, -157.498337), weight: 7} // hawaii
-    ]
-
     var alabama = new google.maps.LatLng(32.806671, -86.791130);
   
     map = new Map(document.getElementById("map"), {
@@ -37,11 +48,13 @@ window.initMap = async function() {
         "rgba(0, 255, 0, 0.15)",
         "rgba(255, 165, 0, 0.35)",
         "rgba(255, 0, 0, 1)",
+        "rgba(139, 0, 0, 1)"
     ];
 
     var heatmap = new google.maps.visualization.HeatmapLayer({
-      data: heatMapData,
-      radius: 5,
+      data: points,
+      radius: 1,
+      maxIntensity: 100,
       dissipating: false,
       gradient: gradient
     });
