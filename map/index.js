01c14660ee641713e0bb10174b779cbae21a074a
kenny <kennycs@protonmail>
2023-09-09 12:58:29 -0400

    Added weights to locations with gradient

--- map/index.js
+++ map/index.js
@@ -9,19 +9,21 @@ script.async = true;
 window.initMap = async function() {
     const { Map } = await google.maps.importLibrary("maps");
 
+    // bigger weight = more red
+
     var heatMapData = [
-      {location: new google.maps.LatLng(32.806671, -86.791130)}, // alabama
-      {location: new google.maps.LatLng(61.370716, -152.404419)}, // alaska
-      {location: new google.maps.LatLng(33.729759, -111.431221)}, // arizona
-      {location: new google.maps.LatLng(34.969704, -92.373123)}, // arkansas
-      {location: new google.maps.LatLng(36.116203, -119.681564)}, // california
-      {location: new google.maps.LatLng(39.059811, -105.311104)}, // colorado
-      {location: new google.maps.LatLng(41.597782, -72.755371)}, // conneticut
-      {location: new google.maps.LatLng(39.318523, -75.507141)}, // delaware 
-      {location: new google.maps.LatLng(38.897438, -77.026817)}, // district of columbia
-      {location: new google.maps.LatLng(27.766279, -81.686783)}, // florida
-      {location: new google.maps.LatLng(33.040619, -83.643074)}, // georgia
-      {location: new google.maps.LatLng(21.094318, -157.498337)} // hawaii
+      {location: new google.maps.LatLng(32.806671, -86.791130), weight: 4}, // alabama
+      {location: new google.maps.LatLng(61.370716, -152.404419), weight: 8}, // alaska
+      {location: new google.maps.LatLng(33.729759, -111.431221), weight: 5}, // arizona
+      {location: new google.maps.LatLng(34.969704, -92.373123), weight: 3}, // arkansas
+      {location: new google.maps.LatLng(36.116203, -119.681564), weight: 5}, // california
+      {location: new google.maps.LatLng(39.059811, -105.311104), weight: 3}, // colorado
+      {location: new google.maps.LatLng(41.597782, -72.755371), weight: 2}, // conneticut
+      {location: new google.maps.LatLng(39.318523, -75.507141), weight: 2}, // delaware 
+      {location: new google.maps.LatLng(38.897438, -77.026817), weight: 3}, // district of columbia
+      {location: new google.maps.LatLng(27.766279, -81.686783), weight: 10}, // florida
+      {location: new google.maps.LatLng(33.040619, -83.643074), weight: 4}, // georgia
+      {location: new google.maps.LatLng(21.094318, -157.498337), weight: 7} // hawaii
     ]
 
     var alabama = new google.maps.LatLng(32.806671, -86.791130);
@@ -30,14 +32,20 @@ window.initMap = async function() {
       center: alabama,
       zoom: 8
     });
-  
+
+    const gradient = [
+        "rgba(0, 255, 0, 0.15)",
+        "rgba(255, 165, 0, 0.35)",
+        "rgba(255, 0, 0, 1)",
+    ];
+
     var heatmap = new google.maps.visualization.HeatmapLayer({
-      data: heatMapData
+      data: heatMapData,
+      radius: 5,
+      dissipating: false,
+      gradient: gradient
     });
 
-    heatmap.set("radius", 5);
-    heatmap.set("dissipating", false);
-  
     heatmap.setMap(map);
 };
 
