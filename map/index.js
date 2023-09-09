4c376b9f56dc09365a01aa1e4bf90681af20ab1b
kenny <kennycs@protonmail>
2023-09-09 10:31:27 -0400

    Map now properly displays on page

--- map/index.js
+++ map/index.js
@@ -1,17 +1,12 @@
 let map;
 
-import { Loader } from "./@googlemaps/js-api-loader"
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
 
-const loader = new Loader({
-    apiKey: "AIzaSyApBGLyOG3tTnJsCjj1_vDeimFp72_xF0Y",
-    version: "weekly",
-});
-  
-loader.load().then(async () => {
-    const { Map } = await google.maps.importLibrary("maps");
-    
-    map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-});
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
