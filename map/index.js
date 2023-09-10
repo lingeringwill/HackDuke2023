636982d3105775114eb375717e95b2c2f8226d04
kenny <kennycs@protonmail>
2023-09-09 21:17:04 -0400

    Added increment and decrement year button

--- map/index.js
+++ map/index.js
@@ -1,9 +1,84 @@
 let map;
+let index = 0;
 let points = [];
+let current;
 
 var script = document.createElement('script');
 script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyApBGLyOG3tTnJsCjj1_vDeimFp72_xF0Y&libraries=visualization&callback=initMap';
 script.async = true;
+
+function createLeft(map) {
+  const controlButton = document.createElement('button');
+
+  controlButton.style.backgroundColor = '#fff';
+  controlButton.style.border = '2px solid #fff';
+  controlButton.style.borderRadius = '3px';
+  controlButton.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlButton.style.color = 'rgb(25,25,25)';
+  controlButton.style.cursor = 'pointer';
+  controlButton.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlButton.style.fontSize = '16px';
+  controlButton.style.lineHeight = '38px';
+  controlButton.style.margin = '8px 30px 22px';
+  controlButton.style.padding = '0 5px';
+  controlButton.style.textAlign = 'center';
+
+  controlButton.textContent = 'Decrement Year';
+  controlButton.title = 'Click to decrement year';
+  controlButton.type = 'button';  
+
+  controlButton.addEventListener('click', () => {
+      if (index != 0) {
+        index--;
+        current.clear();
+
+        for (let i = 0; i < points[index].length; i++) {
+          current.push(points[index][i]);
+        }
+
+        console.log(current);
+      }
+  });
+
+  return controlButton;
+}
+
+function createRight(map) {
+  const controlButton = document.createElement('button');
+
+  controlButton.style.backgroundColor = '#fff';
+  controlButton.style.border = '2px solid #fff';
+  controlButton.style.borderRadius = '3px';
+  controlButton.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlButton.style.color = 'rgb(25,25,25)';
+  controlButton.style.cursor = 'pointer ';
+  controlButton.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlButton.style.fontSize = '16px';
+  controlButton.style.lineHeight = '38px';
+  controlButton.style.margin = '8px 0 22px';
+  controlButton.style.padding = '0 5px';
+  controlButton.style.textAlign = 'center';
+
+  controlButton.textContent = 'Increment Year';
+  controlButton.title = 'Click to increment year';
+  controlButton.type = 'button';  
+
+  controlButton.addEventListener('click', () => {
+      if (index != 27) {
+        index++;
+        current.clear();
+
+        for (let i = 0; i < points[index].length; i++) {
+          current.push(points[index][i]);
+        }
+
+        console.log(current);
+      }
+  });
+
+  return controlButton;
+}
+
 
 window.initMap = async function() {
     const { Map } = await google.maps.importLibrary("maps");
@@ -40,8 +115,16 @@ window.initMap = async function() {
         "rgba(255, 0, 0, 1)",
     ];
 
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-      data: points[0],
+    current = new google.maps.MVCArray([]);
+    
+    for (let i = 0; i < points[index].length; i++) {
+      current.push(points[index][i]);
+    }
+
+    console.log(current);
+
+    let heatmap = new google.maps.visualization.HeatmapLayer({
+      data: current,
       radius: 0.3,
       maxIntensity: 100,
       dissipating: false,
@@ -49,7 +132,16 @@ window.initMap = async function() {
     });
 
     heatmap.setMap(map);
+
+    const controlDiv = document.createElement('div');
+    const leftControl = createLeft(map);
+    const rightControl = createRight(map);
+    controlDiv.appendChild(leftControl);
+    controlDiv.appendChild(rightControl);
+
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(controlDiv);
 };
+
 
 document.head.appendChild(script);
       
